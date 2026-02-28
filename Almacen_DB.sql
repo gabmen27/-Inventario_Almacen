55644bb84afa2b8ebc4503ebd49f88a47b25f14f
gabmen27 <gabrieljesus680@gmail.com>
2026-02-27 20:34:33 -0600

    Se agregaron dos tablas mas
    usuario - ya sea empleado o Admin 
    Movimientos que registrara si es entrada o salida de productos

--- Almacen_DB.sql
+++ Almacen_DB.sql
@@ -24,3 +24,22 @@ create table producto(
     constraint fk_categoria foreign key (id_categoria) references categoria(id_categoria),
     constraint fk_proveedor foreign key (id_proveedor) references proveedor(id_proveedor)
 );
+
+create table usuario (
+    id_usuario int primary key auto_increment,
+    nombre_completo varchar(100) not null,
+    correo varchar(100) unique not null,
+    contrasena varchar(255) not null,
+    rol varchar(20)
+);
+
+create table bitacora_inventario (
+    id_bitacora int primary key auto_increment,
+    id_producto int,
+    id_usuario int,
+    tipo_operacion varchar(20) not null,
+    cantidad int not null,
+    fecha_registro timestamp default current_timestamp,
+    constraint fk_producto_bit foreign key (id_producto) references producto(id_producto),
+    constraint fk_usuario_bit foreign key (id_usuario) references usuario(id_usuario)
+);
