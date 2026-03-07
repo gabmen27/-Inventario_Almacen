27d8c01d55881d14cd2b84362618b2e1e3200ad3
Gabriel Mendez <gabrieljesus680@gmail.com>
2026-03-07 14:30:55 -0600

    Configuracion inicial del servidor y pool de conecxiones

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const mysql = require("mysql2");
 const PORT = 3000;
 app.use(express.json());
 
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "equipo_4",
+  password: "abc123",
+  database: "Almacen_DB",
+});
 
+pool.getConnection((error, conexion) => {
+  if (error) {
+    console.log("Error de conexion a la base de datos");
+  } else {
+    console.log("Conexion exitosa");
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost${PORT}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
